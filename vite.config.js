b3026b3236f8d651c8c82e1ccac2a24e3aec9d87
sandhya1102 <sandhay165@gmail.com>
2025-03-20 15:22:54 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/Blog-Website/",
   plugins: [react(),
     tailwindcss(),
   ],
